388aa8f61787f36d98142af6d785088b025c71dd
louis <louis@PC-LOUIS-OMEN>
2018-05-10 11:14:13 +0200

    added comment on Conversation's class

--- PM6/WYO/src/client/Conversation.java
+++ PM6/WYO/src/client/Conversation.java
@@ -2,14 +2,72 @@ package client;
 
 import java.util.ArrayList;
 
+/**
+ * Classe abstraite représentant une conversation générale.
+ * @author louis
+ *
+ */
 public abstract class Conversation {
-	protected int idConversation;
+
+	protected int idConversation;   
 	protected String nomConversation;
-	protected ArrayList<Message> listMsg;
-	public int getIdConversation() {return this.idConversation;}
-	public String getNomConversation() {return this.nomConversation;}
-	public ArrayList<Message> getListMsg(){return this.listMsg;}
-	public void setIdConversation(int id) {this.idConversation = id;}
-	public void setNomConversation(String nomConversation) {this.nomConversation = nomConversation;}
-	public void setListMsg(ArrayList<Message> listMsg) {this.listMsg = listMsg;}	
+	protected ArrayList<Message> listMsg; //liste contenant les messages de la conversation.	
+	
+	/**
+	 * méthode retournant l'id de la conversation
+	 * @return int idConversation
+	 */
+	public int getIdConversation() {
+		return this.idConversation;
+	}
+	
+	/**
+	 * méthode retournant le nom de la conversation
+	 * @return String nomConversation
+	 */
+	public String getNomConversation() {
+		return this.nomConversation;
+	}
+	
+	/**
+	 * méthode retournant la liste des messages de la conversation
+	 * @return ArrayList<Message>
+	 */
+	public ArrayList<Message> getListMsg(){
+		return this.listMsg;
+	}
+	
+	/**
+	 * méthode mettant à jour l'id de la conversation avec le paramètre.
+	 * @param id
+	 */
+	public void setIdConversation(int id) {
+		this.idConversation = id;
+	}
+	
+	/**
+	 * méthode mettant à jour le nom de la conversation avec le paramètre.
+	 * @param nomConversation
+	 */
+	public void setNomConversation(String nomConversation) {
+		this.nomConversation = nomConversation;
+	}
+	
+	/**
+	 * méthode mettant à jour la liste des messages de la conversation avec le paramètre.
+	 * @param listMsg
+	 */
+	public void setListMsg(ArrayList<Message> listMsg) {
+			this.listMsg = listMsg;
+	}	
+	
+	/**
+	 * méthode ajoutant les éléments en paramètres dans la liste des messages de la conversation.  
+	 * @param listMsg
+	 */
+	public void setListMsg(Message... listMsg) {
+		for(int msg=0; msg<listMsg.length; msg++){
+			this.listMsg.add(listMsg[msg]);
+		}
+	}	
 }
