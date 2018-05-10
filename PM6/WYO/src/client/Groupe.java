ec8f1a31d1bfd5446f95657c2ccbdb72a1479ea1
louis <louis@PC-LOUIS-OMEN>
2018-05-10 12:14:10 +0200

    added comments on Groupe and Message classes

--- PM6/WYO/src/client/Groupe.java
+++ PM6/WYO/src/client/Groupe.java
@@ -3,26 +3,45 @@ package client;
 
 import java.util.ArrayList;
 
+/**
+ * Classe représentant des groupes d'utilisateurs.
+ * @author louis
+ *
+ */
 public class Groupe {
 	
-	private ArrayList<User> list_id_user;
+	private ArrayList<User> list_user;	// liste des utilisateurs membres du groupe.
 	
 	private int id_groupe;
 	
 	private String nom_groupe;
 	
+	/**
+	 * Constructeur de la classe
+	 * @param id
+	 * @param nom
+	 * @param login_createur
+	 */
 	public Groupe(int id, String nom, User login_createur){
 		this.id_groupe = id;
 		this.nom_groupe = nom;
 		ajouter_user(login_createur);
 	}
 	
+	/**
+	 * Méthode ajoutant un utilisateur à la liste des utilisateurs du groupe.
+	 * @param login_user
+	 */
 	public void ajouter_user(User login_user){
-		this.list_id_user.add(login_user);
+		this.list_user.add(login_user);
 	}
 	
+	/**
+	 * Méthode retirant un utilisateur de la liste des utilisateurs du groupe.
+	 * @param login_user
+	 */
 	public void retirer_user(User login_user){
-		this.list_id_user.remove(login_user);
+		this.list_user.remove(login_user);
 	}
 
 }
--- PM6/WYO/src/client/Message.java
+++ PM6/WYO/src/client/Message.java
@@ -1,6 +1,11 @@
 package client;
 import java.sql.*;
 
+/**
+ * Classe représentant le concept de message. Ils sont composés d'un id, d'un libellé, d'un corps, d'une date d'envoi et d'un auteur.
+ * @author louis
+ *
+ */
 public class Message {
 
 	private /*final*/ int idMessage;
@@ -9,7 +14,13 @@ public class Message {
 	private Date date_envoi;
 	private User auteur;
 	
-	
+	/**
+	 * Constructeur de la classe.
+	 * @param libMsg
+	 * @param corpsMsg
+	 * @param dateMsg
+	 * @param u
+	 */
 	public Message(String libMsg, String corpsMsg, Date dateMsg, User u){
 		this.libMessage = libMsg;
 		this.corpsMessage = corpsMsg;
@@ -17,12 +28,67 @@ public class Message {
 		this.auteur = u;
 	}
 	
-	public String getLib() {return libMessage;}
-	public String getCorps() {	return corpsMessage;}
-	public Date getDate_envoi() {return date_envoi;}	
-	public User getAuteur() {return this.auteur;}
-	public void setLib(String libMessage) {this.libMessage = libMessage;}
-	public void setCorps(String corpsMessage) {	this.corpsMessage = corpsMessage;}
-	public void setDate_envoi(Date date_envoi) {this.date_envoi = date_envoi;}
-	public void setAuteur(User a) {this.auteur = a ;}	
+	/**
+	 * Méthode retournant le libellé du message.
+	 * @return
+	 */
+	public String getLib() {
+		return libMessage;
+	}
+	
+	/**
+	 * Méthode retournant le corps du message.
+	 * @return
+	 */
+	public String getCorps() {	
+		return corpsMessage;
+	}
+	
+	/**
+	 * Méthode retournant le date d'envoi du message.
+	 * @return
+	 */
+	public Date getDate_envoi() {
+		return date_envoi;
+	}
+	
+	/**
+	 * Méthode retournant l'auteur du message.
+	 * @return
+	 */
+	public User getAuteur() {
+		return this.auteur;
+	}
+	
+	/**
+	 * Méthode mettant à jour le libellé du message.
+	 * @param libMessage
+	 */
+	public void setLib(String libMessage) {
+		this.libMessage = libMessage;
+	}
+	
+	/**
+	 * Méthode mettant à jour le corps du message.
+	 * @param corpsMessage
+	 */
+	public void setCorps(String corpsMessage) {	
+		this.corpsMessage = corpsMessage;
+	}
+	
+	/**
+	 * Méthode mettant à jour la date d'envoi du message.
+	 * @param date_envoi
+	 */
+	public void setDate_envoi(Date date_envoi) {
+		this.date_envoi = date_envoi;
+	}
+	
+	/**
+	 * Méthode mettant à jour l'auteur du message.
+	 * @param a
+	 */
+	public void setAuteur(User a) {
+		this.auteur = a ;
+	}	
 }
