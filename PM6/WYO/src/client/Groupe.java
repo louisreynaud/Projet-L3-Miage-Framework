1a7540f220b1efd93c636cc47853b1a60fb5b6d3
louis <louis@PC-LOUIS-OMEN>
2018-05-10 10:34:33 +0200

    modified list of user in groups

--- PM6/WYO/src/client/Groupe.java
+++ PM6/WYO/src/client/Groupe.java
@@ -5,23 +5,23 @@ import java.util.ArrayList;
 
 public class Groupe {
 	
-	private ArrayList<String> list_id_user;
+	private ArrayList<User> list_id_user;
 	
 	private int id_groupe;
 	
 	private String nom_groupe;
 	
-	public Groupe(int id, String nom, String login_createur){
+	public Groupe(int id, String nom, User login_createur){
 		this.id_groupe = id;
 		this.nom_groupe = nom;
 		ajouter_user(login_createur);
 	}
 	
-	public void ajouter_user(String login_user){
+	public void ajouter_user(User login_user){
 		this.list_id_user.add(login_user);
 	}
 	
-	public void retirer_user(String login_user){
+	public void retirer_user(User login_user){
 		this.list_id_user.remove(login_user);
 	}
 
--- PM6/WYO/src/client/Groupe_Organisation.java
+++ PM6/WYO/src/client/Groupe_Organisation.java
@@ -2,7 +2,7 @@ package client;
 
 public class Groupe_Organisation extends Groupe {
 
-	public Groupe_Organisation(int id, String nom, String login_createur) {
+	public Groupe_Organisation(int id, String nom, User login_createur) {
 		super(id, nom, login_createur);
 	}
 
