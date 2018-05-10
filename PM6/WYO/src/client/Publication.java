d3a75a46f46457d9bcf043efb02ee345b0d30085
louis <louis@PC-LOUIS-OMEN>
2018-05-10 13:09:11 +0200

    added comments on Evenement, Publication, and Topic classes

--- PM6/WYO/src/client/Publication.java
+++ PM6/WYO/src/client/Publication.java
@@ -1,15 +1,28 @@
 package client;
 
-public class Publication extends Conversation{
+/**
+ * Classe héritant de Conversation. Représente une Publication.
+ * @author louis
+ *
+ */
+public abstract class Publication extends Conversation{
 
 	private int idPublication;
 	private String titrePublication;
 	private String descriptionPublication;
 	
-	public Publication(int id, String nom, String description){
-		this.idConversation = id;
-		this.titrePublication = nom;
-		this.descriptionPublication = description;
-	}
-
+	public int getId(){return this.idPublication;}
+	
+	public String getTitre(){return this.titrePublication;}
+	
+	public String getDescription(){return this.descriptionPublication;}
+	
+	public void setId(int id){this.idPublication = id;}
+	
+	public void setTitre(String titre){this.titrePublication = titre;}
+	
+	public void setDescription(String description){this.descriptionPublication = description;}
+	 
+	
+	
 }
